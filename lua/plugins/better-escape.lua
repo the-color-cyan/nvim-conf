9594400092c33da00bfa50ee642cda55ad0337cd
cyan <bdwllmsn@gmail.com>
2025-06-04 08:41:02 -0500

    better-escape key sequence changed back to kj from jk

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -3,36 +3,36 @@ return {
   "max397574/better-escape.nvim",
   config = function()
     require("better_escape").setup({
-      timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
+      timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape jey and the plugin will ignore it
       default_mappings = false, -- setting this to false removes all the default mappings
       mappings = {
         -- i for insert
         i = {
-          j = {
+          k = {
             -- These can all also be functions
-            k = "<Esc>",
-            --j = "<Esc>",
+            j = "<Esc>",
+            --k = "<Esc>",
           },
         },
         c = {
-          j = {
-            k = "<C-c>",
-            --j = "<C-c>",
+          k = {
+            j = "<C-c>",
+            --k = "<C-c>",
           },
         },
         t = {
-          j = {
-            k = "<C-\\><C-n>",
+          k = {
+            j = "<C-\\><C-n>",
           },
         },
         v = {
-          j = {
-            k = "<Esc>",
+          k = {
+            j = "<Esc>",
           },
         },
         s = {
-          j = {
-            k = "<Esc>",
+          k = {
+            j = "<Esc>",
           },
         },
       },
