94c2e902faa13972da1f1d1ee4d060542e6f950e
cyan <bdwllmsn@gmail.com>
2025-07-13 21:35:24 -0500

    gotta test this shit off of michaelsoft binbows

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,24 @@
+-- nvim-cmp config
+-- https://github.com/hrsh7th/nvim-cmp
+
+local cmp = require("cmp")
+
+return {
+	"hrsh7th/nvim-cmp",
+	opts = {
+		mapping = cmp.mapping.preset.insert({
+			-- Shift+Enter closes menu and inserts new line
+			["<S-CR>"] = function(fallback)
+				if cmp.visible() then
+					cmp.close()
+				end
+				fallback()
+			end,
+
+			-- Tab only handles snippet_forward and not ai_accept
+			["<Tab>"] = cmp.mapping(function(fallback)
+				return LazyVim.cmp.map({ "snippet_forward" }, fallback)()
+			end),
+		}),
+	},
+}
